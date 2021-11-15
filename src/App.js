86d258c2c400386b498182b2f340e0f0b897aa3a
Rohit Pratap Singh <rohitprataps4@gmail.com>
2021-11-15 10:26:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,82 @@
 import "./styles.css";
+import { Grid } from "@material-ui/core";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  IconButton
+} from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
 
 export default function App() {
+  const Product1 = (props) => {
+    const { Product } = props;
+
+    return (
+      <Card>
+        <CardMedia image=" " title={Product.name} />
+        <CardContent>
+          <div>
+            <Typography variant={"h5"} gutterBottom>
+              {Product.name}
+            </Typography>
+            <Typography variant={"h5"} gutterBottom>
+              {Product.price}
+            </Typography>
+            <Typography variant={"h5"} color="textSecondary" gutterBottom>
+              {Product.description}
+            </Typography>
+          </div>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton>
+            <AddShoppingCart />
+          </IconButton>
+        </CardActions>
+      </Card>
+    );
+  };
+
+  const Products = [
+    {
+      id: "1",
+      name: "MacBook",
+      price: "$1230",
+      description: "A 16inch mac book pro"
+    },
+    {
+      id: "2",
+      name: "Shoes",
+      price: "$234",
+      description: "price of the shoes"
+    },
+    {
+      id: "3",
+      name: "mobile",
+      price: "$234",
+      description: "price of the shoes"
+    },
+    {
+      id: "4",
+      name: "nike shoes",
+      price: "$234",
+      description: "price of the shoes"
+    }
+  ];
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Grid container justify={"center"} spacing={4}>
+        {Products.map((Product) => {
+          return (
+            <Grid item key={Product.id} xs={12} sm={6} md={4} xl={3}>
+              <Product1 Product={Product} />
+            </Grid>
+          );
+        })}
+      </Grid>
     </div>
   );
 }
